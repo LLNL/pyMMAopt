ebd2ab5345cafbf8d1434a5da50da9e71d8d2bd5
Miguel <salazardetro1@llnl.gov>
2021-02-09 07:34:52 -0800

    refine example

--- examples/quadratic_pde.py
+++ examples/quadratic_pde.py
@@ -23,17 +23,33 @@ n_vars = args.n_vars
 grid_resol = int(np.sqrt(n_vars))
 
 
-mesh = UnitSquareMesh(grid_resol, grid_resol)
+mesh = Mesh("./corner_mesh.msh")
 DG = FunctionSpace(mesh, "DG", 0)
 x, y = SpatialCoordinate(mesh)
-rho = interpolate(x ** 2 * y ** 3, DG)
+rho = interpolate(Constant(1.0), DG)
+
+solution_pvd = File("quadratic_sol.pvd")
+derivative_pvd = File("derivative.pvd")
+rho_viz = Function(DG)
+der_viz = Function(DG)
+
+
+def deriv_cb(j, dj, rho):
+    with stop_annotating():
+        rho_viz.assign(rho)
+        solution_pvd.write(rho_viz)
+
+        der_viz.assign(dj)
+        derivative_pvd.write(der_viz)
+
 
 J = assemble(Constant(1e4) * rho * rho * dx)
-G = assemble(rho * dx)
+G = AdjFloat(1.0) / assemble(rho * dx)
 m = Control(rho)
-Jhat = ReducedFunctional(J, m)
+Jhat = ReducedFunctional(J, m, derivative_cb_post=deriv_cb)
 Ghat = ReducedFunctional(G, m)
-Glimit = 5.0
+total_area = assemble(Constant(1.0) * dx(domain=mesh), annotate=False)
+Glimit = 1.0 / (total_area / 2.0)
 Gcontrol = Control(G)
 
 
@@ -68,7 +84,7 @@ class ReducedInequality(InequalityConstraint):
 
 problem = MinimizationProblem(
     Jhat,
-    bounds=(1e-6, 1.0),
+    bounds=(1e-5, 100.0),
     constraints=[
         ReducedInequality(Ghat, Glimit, Gcontrol),
     ],
@@ -82,7 +98,7 @@ parameters_mma = {
     "IP": 0,
     "tol": 1e-9,
     "accepted_tol": 1e-8,
+    "norm": "L2",
 }
 solver = MMASolver(problem, parameters=parameters_mma)
 rho_sol = solver.solve()
-File("quadratic_sol.pvd").write(rho_sol)
