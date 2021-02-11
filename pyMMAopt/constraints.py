0ff7b27005569f28ea78e2d138f8819829fe0d35
Miguel <salazardetro1@llnl.gov>
2021-02-11 10:34:16 -0800

    two constraints implemented and passing

--- pyMMAopt/constraints.py
+++ pyMMAopt/constraints.py
@@ -1,4 +1,5 @@
 from pyadjoint.optimization.constraints import InequalityConstraint
+from firedrake import warning
 from pyadjoint import stop_annotating
 
 
@@ -9,11 +10,14 @@ class ReducedInequality(InequalityConstraint):
     For Ghat(m) - Glimit <= 0, pass lower=True
     """
 
-    def __init__(self, Ghat, Glimit, Gcontrol, lower=True):
+    def __init__(self, Ghat, Glimit, Gcontrol, lower=True, normalized=True):
         self.Ghat = Ghat
         self.Glimit = float(Glimit)
         self.Gcontrol = Gcontrol
         self.lower = lower
+        self.normalized = normalized
+        if abs(Glimit) < 1e-4 and normalized:
+            warning(f"Normalized is on with a very small bound {Glimit}")
 
     def function(self, m):
 
@@ -22,9 +26,15 @@ class ReducedInequality(InequalityConstraint):
         print(f"Value: {integral}, Constraint {self.Glimit}")
         with stop_annotating():
             if self.lower:
-                value = -integral / self.Glimit + 1.0
+                if self.normalized:
+                    value = -integral / self.Glimit + 1.0
+                else:
+                    value = -integral + self.Glimit
             else:
-                value = integral / self.Glimit - 1.0
+                if self.normalized:
+                    value = integral / self.Glimit - 1.0
+                else:
+                    value = integral - self.Glimit
         return [value]
 
     def jacobian(self, m):
@@ -33,9 +43,15 @@ class ReducedInequality(InequalityConstraint):
             gradients = self.Ghat.derivative()
             with gradients.dat.vec as v:
                 if self.lower:
-                    v.scale(-1.0 / self.Glimit)
+                    if self.normalized:
+                        v.scale(-1.0 / self.Glimit)
+                    else:
+                        v.scale(-1.0)
                 else:
-                    v.scale(1.0 / self.Glimit)
+                    if self.normalized:
+                        v.scale(1.0 / self.Glimit)
+                    else:
+                        v.scale(1.0)
         return [gradients]
 
     def output_workspace(self):
--- tests/test_restart.py
+++ tests/test_restart.py
@@ -56,5 +56,50 @@ def test_save_with_signal():
     assert errornorm(rho_sol, rho_restart) < 1e-2
 
 
+def test_analytical():
+    mesh = UnitSquareMesh(1, 1, quadrilateral=True)
+    DG = VectorFunctionSpace(mesh, "DG", 0)
+    X = Function(DG)
+    with stop_annotating():
+        X.interpolate(Constant((1.234, 2.345)))
+    x, y = split(X)
+    J = assemble(sqrt(y) * dx)
+    G1 = assemble(((2.0 * x) ** 3 - y) * dx)
+    G2 = assemble(((-1.0 * x + 1.0) ** 3 - y) * dx)
+
+    m = Control(X)
+
+    Jhat = ReducedFunctional(J, m)
+    G1hat = ReducedFunctional(G1, m)
+    G2hat = ReducedFunctional(G2, m)
+    print(f"{G1hat.derivative().dat.data_ro}")
+    print(f"{G2hat.derivative().dat.data_ro}")
+
+    problem = MinimizationProblem(
+        Jhat,
+        bounds=(0.0, 100.0),
+        constraints=[
+            ReducedInequality(G1hat, 1e-5, Control(G1), normalized=False),
+            ReducedInequality(G2hat, 1e-5, Control(G2), normalized=False),
+        ],
+    )
+
+    parameters_mma = {
+        "move": 0.1,
+        "maximum_iterations": 10,
+        "m": 2,
+        "IP": 0,
+        "tol": 1e-9,
+        "accepted_tol": 1e-8,
+        "gcmma": True,
+    }
+    solver = MMASolver(problem, parameters=parameters_mma)
+    rho_opt = solver.solve()
+    assert abs(Jhat(rho_opt) - 0.5443418101973394) < 1e-7
+    solution = Function(DG).interpolate(Constant((0.33332924, 0.29630801)))
+    assert errornorm(rho_opt, solution) < 1e-7
+
+
 if __name__ == "__main__":
-    test_save_with_signal()
+    # test_save_with_signal()
+    test_analytical()
