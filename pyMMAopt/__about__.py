4c6f8b704d1e6260bca25eb54aa3a6087008b8ef
Miguel Salazar de Troya <miguel.salazar@corintis.com>
2023-01-09 09:42:18 +0100

    update versions

--- pyMMAopt/__about__.py
+++ pyMMAopt/__about__.py
@@ -5,5 +5,5 @@ __author__ = "Miguel Salazar de Troya"
 __email__ = "salazardetro1@llnl.gov"
 __copyright__ = "Copyright (c) 2019 {} <{}>".format(__author__, __email__)
 __license__ = "License :: OSI Approved :: MIT License"
-__version__ = "0.0.2"
+__version__ = "0.0.7"
 __status__ = "Development Status :: 4 - Beta"
